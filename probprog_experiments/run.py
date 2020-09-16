b9680763f4b8f8c8dc8a650fb3a1f6cf3c7d4086
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2020-09-16 02:46:59 +0100

    new plotting

--- probprog_experiments/run.py
+++ probprog_experiments/run.py
@@ -16,7 +16,12 @@ import probprog_settings
 import pyprob
 from pyprob.distributions import Empirical
 import torch
+from torchvision.utils import make_grid
 import numpy as np
+
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+marker_size = plt.rcParams['lines.markersize'] ** 2.
 
 probprog_settings.setup()
 
@@ -104,60 +109,115 @@ def plot_trace(trace, file_name=None):
     else:
         plt.show()
 
-def plot_distribution(dists, file_name=None, n_bins=30, num_resample=1000,
-                      trace=None):
+
+def plot_distribution(dists, file_name=None, n_bins=25, num_resample=100, trace=None):
     if isinstance(dists, Empirical):
         dists = [dists]
 
     latents_of_interest = ['host_halo_mass', 'host_halo_redshift',
                            'host_halo_offset_xy', 'number_subhalos',
                            'individual_subhalo_masses',
-                           'position_of_individual_subhalos']
+                           'position_of_individual_subhalos', 'simulated_image_log10']
     marginal_dists = [{} for _ in range(len(dists))]
-    pyprob.set_verbosity(0)
+    # pyprob.set_verbosity(0)
     for i, dist in enumerate(dists):
         if num_resample is not None:
             dist = dist.resample(num_resample)
-        for lat in latents_of_interest:
-            marginal_dists[i]['dist_'+lat] = dist.map(lambda t: t[lat])
-    pyprob.set_verbosity(2)
+        for latent in latents_of_interest:
+            marginal_dists[i][latent] = dist.map(lambda t: t[latent])
+    # pyprob.set_verbosity(2)
 
-    fig = plt.figure(constrained_layout=True)
-    gs = fig.add_gridspec(3, 2)
-    t_color = 'green'
-    c_color = 'red'
+    fig = plt.figure(figsize=(15, 12))
+    gs = fig.add_gridspec(3, 3)
 
-    # TODO deal with latents which have random length
-    for i, lat in enumerate(latents_of_interest[:2] + [latents_of_interest[3]]):
-        if i == 0:
-            ax = fig.add_subplot(gs[0, :])
-        else:
-            ax = fig.add_subplot(gs[1, i-1])
-        for i in range(len(dists)):
-            h, bins, _ = ax.hist(marginal_dists[i]['dist_'+lat].values,
-                                 bins=n_bins, alpha=0.5, density=True, zorder=2)
-        ax.set_xlabel(' '.join(word.capitalize() for word in lat.split('_')))
-        if trace:
-            ax.vlines(trace[lat], 0, np.max(h)*1.05, linestyles='dashed',
-                      zorder=3)
-        ax.grid(True, zorder=1)
+    if trace:
+        ax = fig.add_subplot(gs[0, 0])
+        obs_image = trace['simulated_image_log10']
+        ax.imshow(obs_image)
+        print(obs_image.min(), obs_image.max(), obs_image.shape)
+        ax.set_title('Observed image')
 
-    # plot xy halo offset
-    lat = latents_of_interest[2]
-    xy_values = list(zip(*marginal_dists[i]['dist_' + lat].values))
-    labels = ['$x$ offset', '$y$ offset']
-    for k in range(2):
-        ax = fig.add_subplot(gs[2, k])
-        ax.grid(True, zorder=1)
-        for i in range(len(dists)):
-            label = labels[k]
-            h, bins, _ = ax.hist(xy_values[k], bins=n_bins, alpha=0.5,
-                                 density=True, zorder=2)
-        ax.set_xlabel(label)
-        if trace:
-            ax.vlines(trace[lat][k], 0, np.max(h)*1.05, linestyles='dashed',
-                      zorder=2)
+    ax = fig.add_subplot(gs[0, 1])
+    latent = 'simulated_image_log10'
+    images = np.stack(marginal_dists[0][latent].values)
+    ax.imshow(images.mean(0))
+    ax.set_title('Mean image: {}'.format(dists[0].name))
 
+    if len(dists) > 1:
+        ax = fig.add_subplot(gs[0, 2])
+        latent = 'simulated_image_log10'
+        images = np.stack(marginal_dists[1][latent].values)
+        ax.imshow(images.mean(0))
+        ax.set_title('Mean image: {}'.format(dists[1].name))
+
+        ax = fig.add_subplot(gs[1, 2])
+        latent = 'simulated_image_log10'
+        num_samples = 16
+        sample_images = [torch.from_numpy(marginal_dists[1][latent].sample()).unsqueeze(0) for _ in range(num_samples)]
+        composite_image = make_grid(sample_images, nrow=4, pad_value=sample_images[0].min())[0]
+        ax.imshow(composite_image.numpy())
+        ax.set_title('Sample images: {}'.format(dists[1].name))
+
+    ax = fig.add_subplot(gs[1, 0])
+    latent = 'host_halo_mass'
+    bins = np.histogram(np.hstack([marginal_dists[i][latent].values for i in range(len(dists))]), bins=n_bins)[1]
+    for i in range(len(dists)):
+        ax.hist(marginal_dists[i][latent].values, bins=bins, alpha=0.5, density=True, label=dists[i].name, color=colors[i])
+    ax.set_xlabel('Host halo mass')
+    ax.legend()
+    if trace:
+        ax.axvline(trace[latent], linestyle='dashed', color='black')
+
+    ax = fig.add_subplot(gs[1, 1])
+    latent = 'host_halo_redshift'
+    bins = np.histogram(np.hstack([marginal_dists[i][latent].values for i in range(len(dists))]), bins=n_bins)[1]
+    for i in range(len(dists)):
+        ax.hist(marginal_dists[i][latent].values, bins=bins, alpha=0.5, density=True, color=colors[i])
+    ax.set_xlabel('Host halo redshift')
+    if trace:
+        ax.axvline(trace[latent], linestyle='dashed', color='black')
+
+    ax = fig.add_subplot(gs[2, 0])
+    latent = 'number_subhalos'
+    bins = np.histogram(np.hstack([marginal_dists[i][latent].values for i in range(len(dists))]), bins=n_bins)[1]
+    for i in range(len(dists)):
+        ax.hist(marginal_dists[i][latent].values, bins=bins, alpha=0.5, density=True, color=colors[i])
+    ax.set_xlabel('Number of subhalos')
+    if trace:
+        ax.axvline(trace[latent], linestyle='dashed', color='black')
+
+    ax = fig.add_subplot(gs[2, 1])
+    for i in range(len(dists)):
+        dist_positions = marginal_dists[i]['position_of_individual_subhalos'].values
+        dist_masses = marginal_dists[i]['individual_subhalo_masses'].values
+        for positions, masses in zip(dist_positions, dist_masses):
+            marker_sizes = (np.log10(masses) - 7) * (11-7) * marker_size
+            ax.scatter(positions[:, 0], positions[:, 1], alpha=0.1, color=colors[i], s=marker_sizes)
+    ax.set_xlabel('Subhalo position x')
+    ax.set_ylabel('Subhalo position y')
+    if trace:
+        positions = trace['position_of_individual_subhalos']
+        masses = trace['individual_subhalo_masses']
+        marker_sizes = (np.log10(masses) - 7) * (11-7) * marker_size
+        ax.scatter(positions[:, 0], positions[:, 1], alpha=0.75, color='black', s=marker_sizes)
+
+    ax = fig.add_subplot(gs[2, 2])
+    for i in range(len(dists)):
+        positions = marginal_dists[i]['host_halo_offset_xy'].values_numpy()
+        masses = marginal_dists[i]['host_halo_mass'].values_numpy()
+        marker_sizes = (np.log10(masses) - 12) * marker_size
+        ax.scatter(positions[:, 0], positions[:, 1], alpha=0.2, color=colors[i], s=marker_sizes)
+    ax.set_xlabel('Host halo offset x')
+    ax.set_ylabel('Host halo offset y')
+    if trace:
+        position = trace['host_halo_offset_xy']
+        mass = trace['host_halo_mass']
+        ms = (np.log10(mass) - 12) * marker_size
+        ax.scatter(position[0], position[1], color='black', s=ms)
+        ax.axvline(position[0], linestyle='dashed', color='black')
+        ax.axhline(position[1], linestyle='dashed', color='black')
+
+    plt.tight_layout()
     if file_name:
         print('Plotting to file: {}'.format(file_name))
         plt.savefig(file_name)
@@ -191,19 +251,31 @@ if "posterior" in gen_mode:
                     file_name=posterior_file_name)
 
 ### PLOTTING ###
+prior = None
 if "prior" in plot_types:
+    print('Plotting prior')
     prior = Empirical(file_name=prior_file_name)
     plot_distribution(prior,
                       file_name=fig_directories['prior'] / 'histograms.pdf',
                       num_resample=None)
-    prior.close()
 if "ground_truth" in plot_types:
+    print('Plotting ground truth')
     gt_trace = torch.load(gt_file_name)
     plot_trace(gt_trace,
                file_name=fig_directories['ground_truth'] / 'observed.pdf')
 if "posterior" in plot_types:
+    print('Plotting posterior')
     posterior = Empirical(file_name=posterior_file_name)
-    obs_trace = torch.load(gt_file_name)
-    plot_distribution(posterior, trace=obs_trace,
+    gt_trace = torch.load(gt_file_name)
+
+    if prior is not None:
+        dists = [prior, posterior]
+    else:
+        dists = [posterior]
+
+    plot_distribution(dists, trace=gt_trace,
                       file_name=fig_directories['posterior'] / 'histograms.pdf')
     posterior.close()
+
+if prior is not None:
+    prior.close()
